6a6220c51b62c487bd26a75825f3b570df463116
Sriram1920 <112625430+Sriram1920@users.noreply.github.com>
2022-10-10 14:41:15 +0530

    Create Exe(5) Minimum no.of jumps in steps.py

---
+++ Exe(5) Minimum no.of jumps in steps.py
@@ -0,0 +1,24 @@
+l1=[]
+ele=int(input("Enter the no. of elements: "))
+print("Enter the value: ")
+for i in range(0,ele):
+    val=int(input())
+    l1.append(val)
+
+f=0
+count=0
+try:
+    while True:
+        if count + l1[f] == len(l1):
+            print("output",+count)
+            break
+        if count + l1[f] == len(l1) - 1:
+            print("output",+count + 1)
+            break
+        if count + l1[f]+1== len(l1)-1:
+            print("output",+count + 2)
+            break
+        f += 1
+        count += 1
+except:
+    print("End isn’t reachable...!")
